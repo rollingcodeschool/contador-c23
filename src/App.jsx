45e57f72c2b12535b56e93ade57e18819b90ebcc
Emilse Arias <emilsearias@gmail.com>
2026-04-08 11:15:28 -0300

    agregamos el maquetado del componente teoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Teoria from "./components/Teoria";
+
 function App() {
-  return <h1>Hola mundo</h1>;
+// aqui agrego la logica del componente
+
+  return (
+    <main className='container my-3'>
+      <h1 className="text-center">Hola mundo con react</h1>
+      {/* <Teoria></Teoria> */}
+      <Teoria/>
+      
+    </main>
+  );
 }
 
 export default App;
