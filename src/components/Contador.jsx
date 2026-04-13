ef943c1391797d09cac43f9e0442a40d67f73019
Emilse Arias <emilsearias@gmail.com>
2026-04-13 09:59:50 -0300

    agregamos un ejemplo de funcion que incrementa una variable comun de js

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,9 +1,18 @@
 const Contador = () => {
+// aqui va la logica
+let numero = 0
+
+const incrementar = ()=>{
+    numero = numero +1
+    console.log(numero)
+}
+
     return (
+        // aqui va el html y podria haber un poquito de logica
         <section className="border rounded-3 text-center p-3">
             <h2>Contador</h2>
-            <p className="lead ">1</p>
-            <button className="btn btn-primary me-3">+</button>
+            <p className="lead ">{numero}</p>
+            <button className="btn btn-primary me-3" onClick={incrementar}>+</button>
             <button className="btn btn-danger">-</button>
         </section>
     );
