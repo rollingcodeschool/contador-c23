9501fbe34d5ed92b3e9e904ee21db53b5db9f1aa
Emilse Arias <emilsearias@gmail.com>
2026-04-13 10:25:44 -0300

    agregamos una funcion para actualizar el state dentro del onclick del boton

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,25 +1,29 @@
 import { useState } from "react";
 
 const Contador = () => {
-const [numero, setNumero ] = useState(0)
-// aqui va la logica
-// let numero = 0
+  const [numero, setNumero] = useState(0);
+  // aqui va la logica
+  // let numero = 0
 
-const incrementar = ()=>{
-    // numero = numero +1
-    setNumero(numero + 1)
-    console.log(numero)
-}
+  //   const incrementar = () => {
+  //     //numero++  // numero = numero +1 // esto da error
+  //     setNumero(numero + 1);
+  //   };
 
-    return (
-        // aqui va el html y podria haber un poquito de logica
-        <section className="border rounded-3 text-center p-3">
-            <h2>Contador</h2>
-            <p className="lead ">{numero}</p>
-            <button className="btn btn-primary me-3" onClick={incrementar}>+</button>
-            <button className="btn btn-danger">-</button>
-        </section>
-    );
+  return (
+    // aqui va el html y podria haber un poquito de logica
+    <section className="border rounded-3 text-center p-3">
+      <h2>Contador</h2>
+      <p className="lead ">{numero}</p>
+      <button
+        className="btn btn-primary me-3"
+        onClick={() => setNumero(numero + 1)}
+      >
+        +
+      </button>
+      <button className="btn btn-danger">-</button>
+    </section>
+  );
 };
 
 export default Contador;
